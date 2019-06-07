b2e62bdef8eee8450c75db777e1074acdf81c416
janmonschke <jan.monschke@gmail.com>
2019-06-07 18:09:26 +0200

    add Interstate as fallback for Healine

--- src/preview/components/Headline.tsx
+++ src/preview/components/Headline.tsx
@@ -4,7 +4,7 @@ import { raven } from "../../colors";
 export const Headline = styled.h1`
   font-size: 24px;
   font-weight: 900;
-  font-family: "Interstate-Black";
+  font-family: "Interstate-Black", "Interstate";
   line-height: 1.25;
   letter-spacing: 0.4px;
   text-transform: uppercase;

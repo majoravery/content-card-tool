7586f4b6d50f1e0be1f5ba2719900c5b59a3479c
janmonschke <jan.monschke@gmail.com>
2019-06-07 14:00:46 +0200

    GlobalStyles -> InterstateBlackStyles

--- src/preview/Preview.tsx
+++ src/preview/Preview.tsx
@@ -8,7 +8,7 @@ type PreviewProps = {
   config: InAppConfig;
 };
 
-const GlobalStyles = createGlobalStyle`
+const InterstateBlackStyles = createGlobalStyle`
   @font-face {
     font-family: 'Interstate-Black';
     src: local('☁'),
@@ -43,7 +43,7 @@ export const PreviewContent: React.FC<PreviewProps> = ({ config }) => {
   const Template = config.kind === Horizontal ? HorizontalTemplate : undefined;
   return (
     <React.Fragment>
-      <GlobalStyles />
+      <InterstateBlackStyles />
       <link
         rel="stylesheet"
         href="https://style.sndcdn.com/css/interstate-9a7e854c95d1b49e3807d5ea457e6e538034dc55.css"

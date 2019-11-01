86cdcc9e4486f87b4092e334b138073e70794026
Jan Monschke <janmonschke@fastmail.com>
2019-11-01 09:29:18 +0100

    remove maxLength (was cutting off liquig statements

--- src/editor/Form.tsx
+++ src/editor/Form.tsx
@@ -60,7 +60,6 @@ const Form: React.FC<EditorProps> = ({ config, setConfig }) => {
         <Input
           label="Text"
           value={config.buttons.primary.text}
-          maxLength={95}
           onChange={setButtonConfig(config, setConfig, {
             name: "primary",
             prop: "text"
@@ -86,7 +85,6 @@ const Form: React.FC<EditorProps> = ({ config, setConfig }) => {
             <Input
               label="Text"
               value={config.buttons.secondary.text}
-              maxLength={95}
               onChange={setButtonConfig(config, setConfig, {
                 name: "secondary",
                 prop: "text"
